3c31e77cdfbb3c97b3fd1899f6e62e388637e991
Pawlash <mrpawel321@gmail.com>
2024-08-17 16:39:31 +0200

    fix: image behaviour when on "discord" section

--- main.js
+++ main.js
@@ -1,22 +1,28 @@
+const nameElement = document.querySelector("#name");
+
 function changeImage(newSrc) {
   document.querySelector("#header-image").src = newSrc;
 }
 
 function resetImage() {
-  document.querySelector("#header-image").src = 'images/caracal.avif';
+  const isDiscordShown = nameElement.textContent === 'Hi, my name is pawlash.';
+  if (isDiscordShown) {
+    document.querySelector("#header-image").src = 'images/discord.avif';
+  } else {
+    document.querySelector("#header-image").src = 'images/caracal.avif';
+  }
 }
 
 document.querySelector("#discord-link").addEventListener('click', function(event) {
   event.preventDefault();
-  
-  const name = document.querySelector("#name");
-  const isDiscordShown = name.textContent === 'Hi, my name is pawlash.';
+
+  const isDiscordShown = nameElement.textContent === 'Hi, my name is pawlash.';
 
   if (isDiscordShown) {
-    resetImage();
-    name.textContent = 'Hi, my name is Paweł.';
+    changeImage('images/caracal.avif');
+    nameElement.textContent = 'Hi, my name is Paweł.';
   } else {
     changeImage('images/discord.avif');
-    name.textContent = 'Hi, my name is pawlash.';
+    nameElement.textContent = 'Hi, my name is pawlash.';
   }
 });
