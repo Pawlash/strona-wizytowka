247eef3d164f5a63f3baf8eb7ad40062202a835c
Pawlash <mrpawel321@gmail.com>
2024-12-09 19:25:25 +0100

    feat: Prettier formatting, preloading, eventListeners instead of onMouse

--- main.js
+++ main.js
@@ -1,28 +1,56 @@
+// preload all images, so hover effect will be smooth
+window.addEventListener("load", () => {
+  const images = [
+    "images/discord.avif",
+    "images/caracal.avif",
+    "images/projects/currency-converter.avif",
+    "images/projects/password-validator.avif",
+    "images/projects/simple-calculator.avif",
+    "images/projects/simple-todo-list.avif",
+  ];
+
+  images.forEach((src) => {
+    const img = new Image();
+    img.src = src;
+  });
+});
+
+const buttons = document.querySelectorAll(".button");
+
+buttons.forEach((button) => {
+  button.addEventListener("mouseover", function () {
+    const imageSrc = button.getAttribute("data-image");
+    changeImage(imageSrc);
+  });
+
+  button.addEventListener("mouseout", function () {
+    resetImage();
+  });
+});
+
 const nameElement = document.querySelector("#name");
+let isDiscordShown = false;
 
 function changeImage(newSrc) {
   document.querySelector("#header-image").src = newSrc;
 }
 
 function resetImage() {
-  const isDiscordShown = nameElement.textContent === 'Hi, my name is pawlash.';
-  if (isDiscordShown) {
-    document.querySelector("#header-image").src = 'images/discord.avif';
-  } else {
-    document.querySelector("#header-image").src = 'images/caracal.avif';
-  }
+  changeImage(isDiscordShown ? "images/discord.avif" : "images/caracal.avif");
 }
 
-document.querySelector("#discord-link").addEventListener('click', function(event) {
-  event.preventDefault();
+document
+  .querySelector("#discord-link")
+  .addEventListener("click", function (event) {
+    event.preventDefault();
 
-  const isDiscordShown = nameElement.textContent === 'Hi, my name is pawlash.';
+    if (isDiscordShown) {
+      changeImage("images/caracal.avif");
+      nameElement.textContent = "Hi, my name is Paweł.";
+    } else {
+      changeImage("images/discord.avif");
+      nameElement.textContent = "Hi, my name is pawlash.";
+    }
 
-  if (isDiscordShown) {
-    changeImage('images/caracal.avif');
-    nameElement.textContent = 'Hi, my name is Paweł.';
-  } else {
-    changeImage('images/discord.avif');
-    nameElement.textContent = 'Hi, my name is pawlash.';
-  }
-});
+    isDiscordShown = !isDiscordShown;
+  });
